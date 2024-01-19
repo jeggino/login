e6e6d5cc5824d5f48bd245597babd2037759705b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-19 11:34:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,12 @@ authenticator = stauth.Authenticate(
 )
 
 authenticator.login('Login', 'main')
+
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')

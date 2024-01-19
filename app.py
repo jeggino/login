842f1384eae4e4d6e2626f973ab8321f7677d514
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-19 12:08:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,33 +4,31 @@ from yaml.loader import SafeLoader
 import streamlit_authenticator as stauth
 
 
-_RELEASE = True
 
-if not _RELEASE:
-    # hashed_passwords = Hasher(['abc', 'def']).generate()
+# hashed_passwords = Hasher(['abc', 'def']).generate()
 
-    # Loading config file
-    with open('config.yaml') as file:
-        config = yaml.load(file, Loader=SafeLoader)
+# Loading config file
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-    # Creating the authenticator object
-    authenticator = Authenticate(
-        config['credentials'],
-        config['cookie']['name'], 
-        config['cookie']['key'], 
-        config['cookie']['expiry_days'],
-        config['preauthorized']
-    )
+# Creating the authenticator object
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'], 
+    config['cookie']['key'], 
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
-    # creating a login widget
-    authenticator.login('Login', 'main')
-    if st.session_state["authentication_status"]:
-        authenticator.logout('Logout', 'main')
-        st.write(f'Welcome *{st.session_state["name"]}*')
-        st.title('Some content')
-    elif st.session_state["authentication_status"] is False:
-        st.error('Username/password is incorrect')
-    elif st.session_state["authentication_status"] is None:
-        st.warning('Please enter your username and password')
+# creating a login widget
+authenticator.login('Login', 'main')
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
 
    

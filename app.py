55f40309faea8bdca8db1df610a6f289496b88e7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-19 12:41:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,48 @@
-import yaml
+# import yaml
+# import streamlit as st
+# from yaml.loader import SafeLoader
+# import streamlit_authenticator as stauth
+
+
+
+# # Loading config file
+# with open('config.yaml') as file:
+#     config = yaml.load(file, Loader=SafeLoader)
+
+# # Creating the authenticator object
+# authenticator = stauth.Authenticate(
+#     config['credentials'],
+#     config['cookie']['name'], 
+#     config['cookie']['key'], 
+#     config['cookie']['expiry_days'],
+#     config['preauthorized']
+# )
+
+# # creating a login widget
+# authenticator.login('', 'sidebar')
+# if st.session_state["authentication_status"]:
+#     # authenticator.logout('Logout', 'main')
+#     st.write(f'Welcome *{st.session_state["name"]}*')
+#     st.title('Some content')
+# elif st.session_state["authentication_status"] is False:
+#     st.error('Username/password is incorrect')
+# elif st.session_state["authentication_status"] is None:
+#     st.warning('Please enter your username and password')
+
 import streamlit as st
-from yaml.loader import SafeLoader
-import streamlit_authenticator as stauth
+from streamlit_login_auth_ui.widgets import __login__
 
+__login__obj = __login__(auth_token = "courier_auth_token", 
+                    company_name = "Shims",
+                    width = 200, height = 250, 
+                    logout_button_name = 'Logout', hide_menu_bool = False, 
+                    hide_footer_bool = False, 
+                    lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
 
+LOGGED_IN = __login__obj.build_login_ui()
 
-# Loading config file
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+if LOGGED_IN == True:
 
-# Creating the authenticator object
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'], 
-    config['cookie']['key'], 
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
-
-# creating a login widget
-authenticator.login('', 'sidebar')
-if st.session_state["authentication_status"]:
-    # authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
+    st.markown("Your Streamlit Application Begins here!")
 
    

5870b6aaa79761d6c7c15a0b0e98f8fb4c22d568
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-20 10:28:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,5 @@
-# import yaml
 import streamlit as st
-# from yaml.loader import SafeLoader
-# import streamlit_authenticator as stauth
-
-
-
-# # Loading config file
-# with open('config.yaml') as file:
-#     config = yaml.load(file, Loader=SafeLoader)
-
-# # Creating the authenticator object
-# authenticator = stauth.Authenticate(
-#     config['preauthorized']
-# )
-
-# # creating a login widget
-# authenticator.login('', 'sidebar')
-# if st.session_state["authentication_status"]:
-#     # authenticator.logout('Logout', 'main')
-#     st.write(f'Welcome *{st.session_state["name"]}*')
-#     st.title('Some content')
-# elif st.session_state["authentication_status"] is False:
-#     st.error('Username/password is incorrect')
-# elif st.session_state["authentication_status"] is None:
-#     st.warning('Please enter your username and password')
+import .credencials
 
 dict = {
     "roberto":'ciao',

29ace9105233e0e0aa3f863f97be376b772350b9
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-21 10:04:40 +0100

    Update lof_fun.py

--- lof_fun.py
+++ lof_fun.py
@@ -1,1 +1,25 @@
-ff
+import streamlit as st
+from credencials import dict
+
+def login():
+  placeholder = st.empty()
+  container =  placeholder.container(height=None, border=True)
+  id_input = container.text_input( "Enter your user name here",value="")
+  id_input_2 = container.text_input( "Enter your password here",type="password")
+  
+  if container.button('Login'):
+      if id_input in dict.keys():
+          
+          if dict[id_input]['password'] == id_input_2:
+              placeholder.empty()
+              
+              st.title(f"HI :blue[{dict[id_input]['name']}]")
+              if st.button("Logout"):
+                  st.rerun()
+              
+          else:
+              container.write('Password incorrect')
+              st.stop()
+      else:
+          container.write("User name incorrect")
+          st.stop()

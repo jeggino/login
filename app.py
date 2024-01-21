db1a400f8b14c4f034580cac2fcb24d2a09c3d26
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-21 09:03:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 from credencials import dict
 
-with st.container(height=None, border=True):
-    id_input = st.text_input( "Enter your user name here",value="")
-    id_input_2 = st.text_input( "Enter your password here",type="password")
+container =  st.container(height=None, border=True):
+id_input = container.text_input( "Enter your user name here",value="")
+id_input_2 = container.text_input( "Enter your password here",type="password")
 
-    if st.button('Login'):
-        if id_input in dict.keys():
+if st.button('Login'):
+    if id_input in dict.keys():
+        
+        if dict[id_input]['password'] == id_input_2:
+            container.write(f"Hi {dict[id_input]['name']}")
             
-            if dict[id_input]['password'] == id_input_2:
-                st.write(f"Hi {dict[id_input]['name']}")
-                
-            else:
-                st.write('Password incorrect')
         else:
-            st.write("User name incorrect")
+            container.write('Password incorrect')
+    else:
+        st.write("User name incorrect")

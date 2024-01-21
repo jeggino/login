6e8223fffbb0c8115296e257f64fba1f7fe48ec5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-21 10:42:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from log_fun import *
 
 login()
         
-with st.sidebar():
+with st.sidebar:
         genre = st.radio("What's your favorite movie genre",
                          [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
                          captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])

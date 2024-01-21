295f8a047b8f1b38193bb2996c798a9f82de6891
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-21 15:04:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ login()
 if st.button("Logout"):
         st.cache_resource.clear()
         st.rerun()
-with st.container(height=120):
-        st.image("https://terschelling-cdn.travelbase.nl/image-transforms/logo/500x500/f30402d250ee0d24b57be8e69299e1f9.png")
+        
+st.sidebar.image("https://terschelling-cdn.travelbase.nl/image-transforms/logo/500x500/f30402d250ee0d24b57be8e69299e1f9.png")
         
 uploaded_file = st.file_uploader("")
 

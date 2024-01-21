515c619d9c44f0d573bc085d88994990976af177
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-21 16:32:39 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,3 +8,18 @@ if st.button("Logout"):
         st.rerun()
 
 "---"
+
+from deta import Deta
+
+data_key = "a0eengonb7b_UCVkWd6CJS5ccGoKFzNCYj6UnMeRcF5Y"
+
+# Connect to Deta Base with your Data Key
+deta = Deta(data_key)
+
+
+db = deta.Base("elskenecologie_2024-db")
+
+"Here's everything stored in the database:"
+
+db_content = db.fetch().items
+st.dataframe(db_content)

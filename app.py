b65ad798d79fdb94d5f104d8d9ccd9c400ec3d0b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-21 09:04:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ container =  st.container(height=None, border=True)
 id_input = container.text_input( "Enter your user name here",value="")
 id_input_2 = container.text_input( "Enter your password here",type="password")
 
-if st.button('Login'):
+if container.button('Login'):
     if id_input in dict.keys():
         
         if dict[id_input]['password'] == id_input_2:
-            container.write(f"Hi {dict[id_input]['name']}")
+            st.write(f"Hi {dict[id_input]['name']}")
             
         else:
             container.write('Password incorrect')
     else:
-        st.write("User name incorrect")
+        container.write("User name incorrect")

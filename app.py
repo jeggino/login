6615627a18143383e0242cf85255e8e759722ac7
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-21 12:43:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,3 +23,15 @@ st.map(df,
     longitude='lng'
       )
 
+import altair as alt
+
+source = df.groupby("sciName",as_index=False).size()
+
+base = alt.Chart(source).encode(
+    x='sciName',
+    y="size",
+    text='size'
+)
+
+base.mark_bar() + base.mark_text(align='left', dx=2)
+

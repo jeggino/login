84ba1604736755fe9107788d486393c1af71fbd6
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-21 11:06:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,29 @@ if st.button("Logout"):
         st.cache_resource.clear()
         st.rerun()
         
-with st.sidebar:
-        genre = st.radio("What's your favorite movie genre",
-                         [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
-                         captions = ["Laugh out loud.", "Get the popcorn.", "Never stop learning."])
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is None:
+        st.stop()
 
+import pandas as pd
 
-st.write(f'You selected {genre}.')
+data_df = pd.DataFrame(
+    {
+        "apps": [
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/ef9a7627-13f2-47e5-8f65-3f69bb38a5c2/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/31b99099-8eae-4ff8-aa89-042895ed3843/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/6a399b09-241e-4ae7-a31f-7640dc1d181e/Home_Page.png",
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "apps": st.column_config.ImageColumn(
+            "Preview Image", help="Streamlit app preview screenshots"
+        )
+    },
+    hide_index=True,
+)

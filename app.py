af2f86821bf03bd8b4da23491d9dcfd2a74acdb8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-21 09:15:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from credencials import dict
 
-container =  st.container(height=None, border=True)
+placeholder = st.empty()
+container =  placeholder.container(height=None, border=True)
 id_input = container.text_input( "Enter your user name here",value="")
 id_input_2 = container.text_input( "Enter your password here",type="password")
 
@@ -9,6 +10,7 @@ if container.button('Login'):
     if id_input in dict.keys():
         
         if dict[id_input]['password'] == id_input_2:
+            placeholder.empty()
             st.write(f"Hi {dict[id_input]['name']}")
             
         else:

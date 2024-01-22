d7a5a29a6fea96195510a0e371e8b2f68615fdc2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-01-22 12:41:01 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -35,9 +35,7 @@ layer = pdk.Layer(
     opacity=0.8,
     stroked=True,
     filled=True,
-    radius_scale=6,
-    radius_min_pixels=1,
-    radius_max_pixels=100,
+    radius_scale=50,
     line_width_min_pixels=1,
     get_position=["lng","lat"],
     get_fill_color=[255, 140, 0],
@@ -45,7 +43,7 @@ layer = pdk.Layer(
 )
 
 # Set the viewport location
-view_state = pdk.ViewState(latitude=db_content.lat.mean(), longitude=db_content.lng.mean(), zoom=9, bearing=0, pitch=0)
+view_state = pdk.ViewState(latitude=db_content["lat"].mean(), longitude=db_content["lng"].mean(), zoom=9, bearing=0, pitch=0)
 
 # Render
 r = pdk.Deck(layers=[layer], 

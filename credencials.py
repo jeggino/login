1e0e31d4e38313566bacb3013556b8f4d51d9765
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-06 14:29:09 +0100

    Update and rename credencials.py to credencials.yalm

--- credencials.py
+++
@@ -1,6 +0,0 @@
-dict = {
-  'daan@elskenecologie.nl': {'name': 'Daan',
-              'password':'elskenecologie_2024'},
-  'jeggino@gmail.com' : {'name': 'Luigi',
-             'password': 'elskenecologie_2024'}
-}

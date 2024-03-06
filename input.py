937ebf6db7b1e95e43ff7b653496b1515bd319ad
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-06 14:32:49 +0100

    Update input.py

--- input.py
+++ input.py
@@ -1,63 +1,16 @@
 import streamlit as st
-from log_fun import *
+from streamlit_login_auth_ui.widgets import __login__
 
-login()
+__login__obj = __login__(auth_token = "courier_auth_token", 
+                    company_name = "Shims",
+                    width = 200, height = 250, 
+                    logout_button_name = 'Logout', hide_menu_bool = False, 
+                    hide_footer_bool = False, 
+                    lottie_url = 'https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
 
-if st.button("Logout"):
-        st.cache_resource.clear()
-        st.rerun()
+LOGGED_IN = __login__obj.build_login_ui()
 
-"---"
-          
-uploaded_file = st.file_uploader("")
+if LOGGED_IN == True:
 
-if uploaded_file is None:
-        st.stop()
-
-import pandas as pd
-from geopy.geocoders import Nominatim
-
-df = pd.read_csv(uploaded_file)
-
-
-geolocator = Nominatim(user_agent="user_agent")
-df['Adres'] = df['Adres'] + " " + df['Stad']
-df['lat'] = df['Adres'].apply(lambda x: geolocator.geocode(x).latitude)
-df['lng'] = df['Adres'].apply(lambda x: geolocator.geocode(x).longitude)
-
-#--------------
-from deta import Deta
-
-data_key = "a0eengonb7b_UCVkWd6CJS5ccGoKFzNCYj6UnMeRcF5Y"
-
-# Data to be written to Deta Base
-with st.form("form"):
-    st.dataframe(df)
-    submitted = st.form_submit_button("Store in database")
-
-
-# Connect to Deta Base with your Data Key
-deta = Deta(data_key)
-
-# Create a new database "example-db"
-# If you need a new database, just use another name.
-db = deta.Base("elskenecologie_2024-db")
-
-# If the user clicked the submit button,
-# write the data from the form to the database.
-# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
-if submitted:
-        for row in df.to_dict('records'):
-                db.put(row)
-
-"---"
-"Here's everything stored in the database:"
-# This reads all items from the database and displays them to your app.
-# db_content is a list of dictionaries. You can do everything you want with it.
-db_content = db.fetch(limit=None).items
-st.dataframe(db_content)
-
-
-
-
+    st.markown("Your Streamlit Application Begins here!")
 

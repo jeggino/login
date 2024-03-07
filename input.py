82382d5edd237a99ba5bd7d2130d3871916c787d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-07 09:42:48 +0100

    Update input.py

--- input.py
+++ input.py
@@ -20,8 +20,9 @@ authenticator.login()
 
 if st.session_state["authentication_status"]:
     authenticator.logout()
+    with open('config.yaml', 'w') as file:
+        yaml.dump(config, file, default_flow_style=False)
     st.write(f'Welcome *{st.session_state["name"]}*')
-    st.write(st.session_state["username"])
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:

58c87ecc8c0096b19903a74006865dd8036dc624
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-07 08:51:31 +0100

    Update input.py

--- input.py
+++ input.py
@@ -17,12 +17,11 @@ authenticator = stauth.Authenticate(
 
 authenticator.login(location="sidebar")
 
-st.write("dhjsskjh")
-# if st.session_state["authentication_status"]:
-#     authenticator.logout()
-#     st.write(f'Welcome *{st.session_state["name"]}*')
-#     st.title('Some content')
-# elif st.session_state["authentication_status"] is False:
-#     st.error('Username/password is incorrect')
-# elif st.session_state["authentication_status"] is None:
-#     st.warning('Please enter your username and password')
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')

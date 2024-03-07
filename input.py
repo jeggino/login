9f095053b107f2365c9ba383f717206cd5b8d230
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-03-07 09:15:11 +0100

    Update input.py

--- input.py
+++ input.py
@@ -21,11 +21,12 @@ authenticator.login(location="sidebar")
 if st.session_state["authentication_status"]:
     authenticator.logout()
     st.write(f'Welcome *{st.session_state["name"]}*')
+    st.write(st.session_state["username"])
     st.title('Some content')
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
 
-st.write(st.session_state["username"])
+
 
